d6c27266c77c88a1d923998837f0c202ce9269bd
RanAndRun <ranhaber123@gmail.com>
2024-07-17 23:41:06 +0200

    Full Game

--- Main.py
+++ Main.py
@@ -1,54 +1,73 @@
 from Card import Card
 from Deck import Deck
-class Main:
 
 
-    def find_count(cards_list : list[Card]):
-        count = 0
-        got_an_ace = False
-        for card in cards_list:
-            if 2 <= card.num <= 10:
-                count += card.num
-            elif card.num > 10:
-                count += 10
+def find_count(cards_list : list[Card]):
+    count = 0
+    got_an_ace = False
+    for card in cards_list:
+        if 2 <= card.num <= 10:
+            count += card.num
+        elif card.num > 10:
+            count += 10
+        else:
+            # ace
+            if count <= 10:
+                got_an_ace = True
+                count += 11
             else:
-                # ace
-                if count <= 10:
-                    got_an_ace = True
-                    count += 11
-                else:
-                    count += 1
-            if got_an_ace and count > 21:
-                count -= 10
-                got_an_ace = False
-        
-        return count
-                
-
-
-    def turn_handler(players_hands, curr_hand_index):
-        while(find_count(players_cards[0]) < 21) or splits_finished:
-            print("make an action")
-            action = input()
-            if action == 'h':
-                players_cards[0].append(deck.pop_card())
-                print(players_cards[-1])
-                print(find_count(players_cards[0]))
-            elif action == 's':
-                print(f"Stayed on {find_count(players_cards)}")
-                break
-            elif action == "d":
-                print("Double")
-                players_money -= money_put_in_hand
-                money_put_in_hand *= 2
-                players_cards.append(deck.pop_card())
-                print(players_cards[-1])
-                print(find_count(players_cards))
-                break
-            elif action == "ss" and split_allowed:
-                splits_finished = True
+                count += 1
+        if got_an_ace and count > 21:
+            count -= 10
+            got_an_ace = False
     
+    return count
+            
 
+def is_split_possible(hand : list):
+    return len(hand) == 2 and hand[0].num == hand[1].num
+
+def is_double_possible(hand : list):
+    return len(hand) == 2
+
+def turn_handler(players_hands : list, deck : Deck, players_money: int, money_put_in_hand: int):
+    curr_hand_index = 0
+    split_allowed = True
+    while len(players_hands) > curr_hand_index:
+        if(find_count(players_hands[curr_hand_index]) > 21):
+            curr_hand_index += 1
+            continue
+        print("make an action")
+        print("player has ", find_count(players_hands[curr_hand_index]))
+        action = input()
+        if action == 'h':
+            split_allowed = False
+            players_hands[curr_hand_index].append(deck.pop_card())
+            print(players_hands[curr_hand_index][-1])
+            print(find_count(players_hands[curr_hand_index]))
+        elif action == 's':
+            print(f"Stayed on {find_count(players_hands[curr_hand_index])}")
+            curr_hand_index +=1
+        elif action == "d" and is_double_possible(players_hands[curr_hand_index]):
+            print("Double")
+            players_money -= money_put_in_hand
+            money_put_in_hand *= 2
+            players_hands.append(deck.pop_card())
+            print(players_hands[-1])
+            print(find_count(players_hands[curr_hand_index]))
+            curr_hand_index +=1
+        elif action == "ss" and is_split_possible(players_hands[curr_hand_index]) and money_put_in_hand <= players_money:
+            players_money -= money_put_in_hand
+            players_hands.append([players_hands[curr_hand_index].pop()])
+            players_hands[curr_hand_index].append(deck.pop_card())
+            players_hands[curr_hand_index + 1].append(deck.pop_card())
+
+            print(players_hands)
+
+    return players_money
+
+
+class Main:
     cards = []
     for i in range(1, 14):
         for j in range(4):
@@ -71,7 +90,7 @@ class Main:
         while True:
             print("player has ", players_money)
             money_put_in_hand = int(input("how much money on hand"))
-            if(money_put_in_hand < players_money):
+            if(money_put_in_hand <= players_money):
                 players_money -= money_put_in_hand
                 break
             else:
@@ -79,7 +98,7 @@ class Main:
         gameOn = True
         while(gameOn):     
             # player and dealer recieve 2 cards.
-            players_cards = []
+            players_hands = []
             dealer_cards = []
 
             split_allowed = True
@@ -87,50 +106,44 @@ class Main:
 
             deck = Deck(cards)
             deck.shuffle_cards()
-            players_cards.append([deck.pop_card()])
+            players_hands.append([deck.pop_card()])
             dealer_cards.append(deck.pop_card())
-            players_cards[0].append(deck.pop_card())
+            players_hands[0].append(deck.pop_card())
             dealer_cards.append(deck.pop_card())
 
             print("Players Cards :")
-            print(players_cards[0][0], "|", players_cards[0][1])
-            print(find_count(players_cards[0]))
+            print(players_hands[0][0], "|", players_hands[0][1])
+            print(find_count(players_hands[0]))
             print("\n\nDealer face up card:")
             print(dealer_cards[0], "|")
             print(dealer_cards[0].get_value_of_card())
 
             #players turn
+            players_money = turn_handler(players_hands, deck, players_money, money_put_in_hand)
 
-
-
-
-
-
-        
-
-
-            if find_count(players_cards[0]) > 21:
-                # player busted
-                print("player busted")
-            else:
-                print("dealer has", dealer_cards[0], "|", dealer_cards[1], "\nCount is", find_count(dealer_cards))
-                while(find_count(dealer_cards) < 17):
-                    dealer_cards.append(deck.pop_card())
-                    print(dealer_cards[-1])
-                    print(find_count(dealer_cards))
-                if find_count(dealer_cards) > 21 or find_count(dealer_cards) < find_count(players_cards):
-                    print("player won")
-                    players_money += money_put_in_hand * 2
-                elif find_count(dealer_cards[0]) > find_count(players_cards[0]):
-                    print("dealer won")
+            for hand in players_hands:
+                if find_count(hand) > 21:
+                    # player busted
+                    print("player busted")
                 else:
-                    print("push")
-                    players_money += money_put_in_hand
-            
-            print("player has ", players_money)
+                    print("dealer has", dealer_cards[0], "|", dealer_cards[1], "\nCount is", find_count(dealer_cards))
+                    while(find_count(dealer_cards) < 17):
+                        dealer_cards.append(deck.pop_card())
+                        print(dealer_cards[-1])
+                        print(find_count(dealer_cards))
+                    if find_count(dealer_cards) > 21 or find_count(dealer_cards) < find_count(hand):
+                        print("player won")
+                        players_money += money_put_in_hand * 2
+                    elif find_count(dealer_cards) > find_count(hand):
+                        print("dealer won")
+                    else:
+                        print("push")
+                        players_money += money_put_in_hand
+                
+                print("player has ", players_money)
 
 
-            gameOn = False
+                gameOn = False
 
 
         
